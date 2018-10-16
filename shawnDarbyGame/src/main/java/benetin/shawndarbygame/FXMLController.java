9b8ec2b3012b8e4688c134d086bcccd8774551c8
shawnb58 <shawnb58@BHSCUR149013ITW.kprdsb.schools.local>
2018-10-16 13:38:00 -0400

    Declared all the rectangles in the ten by ten grid

--- shawnDarbyGame/src/main/java/benetin/shawndarbygame/FXMLController.java
+++ shawnDarbyGame/src/main/java/benetin/shawndarbygame/FXMLController.java
@@ -1,3 +1,4 @@
+//Wood Block Game
 package benetin.shawndarbygame;
 
 import java.net.URL;
@@ -6,20 +7,220 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.shape.Rectangle;
 
 public class FXMLController implements Initializable {
-    
+
     @FXML
-    private Label label;
-    
+    private Rectangle rec00;
+    @FXML
+    private Rectangle rec01;
+    @FXML
+    private Rectangle rec02;
+    @FXML
+    private Rectangle rec03;
+    @FXML
+    private Rectangle rec04;
+    @FXML
+    private Rectangle rec05;
+    @FXML
+    private Rectangle rec06;
+    @FXML
+    private Rectangle rec07;
+    @FXML
+    private Rectangle rec08;
+    @FXML
+    private Rectangle rec09;
+    @FXML
+    private Rectangle rec10;
+    @FXML
+    private Rectangle rec11;
+    @FXML
+    private Rectangle rec12;
+    @FXML
+    private Rectangle rec13;
+    @FXML
+    private Rectangle rec14;
+    @FXML
+    private Rectangle rec15;
+    @FXML
+    private Rectangle rec16;
+    @FXML
+    private Rectangle rec17;
+    @FXML
+    private Rectangle rec18;
+    @FXML
+    private Rectangle rec19;
+    @FXML
+    private Rectangle rec2;
+    @FXML
+    private Rectangle rec20;
+    @FXML
+    private Rectangle rec21;
+    @FXML
+    private Rectangle rec22;
+    @FXML
+    private Rectangle rec23;
+    @FXML
+    private Rectangle rec24;
+    @FXML
+    private Rectangle rec25;
+    @FXML
+    private Rectangle rec26;
+    @FXML
+    private Rectangle rec27;
+    @FXML
+    private Rectangle rec28;
+    @FXML
+    private Rectangle rec29;
+    @FXML
+    private Rectangle rec30;
+    @FXML
+    private Rectangle rec31;
+    @FXML
+    private Rectangle rec32;
+    @FXML
+    private Rectangle rec33;
+    @FXML
+    private Rectangle rec34;
+    @FXML
+    private Rectangle rec35;
+    @FXML
+    private Rectangle rec36;
+    @FXML
+    private Rectangle rec37;
+    @FXML
+    private Rectangle rec38;
+    @FXML
+    private Rectangle rec39;
+    @FXML
+    private Rectangle rec40;
+    @FXML
+    private Rectangle rec41;
+    @FXML
+    private Rectangle rec42;
+    @FXML
+    private Rectangle rec43;
+    @FXML
+    private Rectangle rec44;
+    @FXML
+    private Rectangle rec45;
+    @FXML
+    private Rectangle rec46;
+    @FXML
+    private Rectangle rec47;
+    @FXML
+    private Rectangle rec48;
+    @FXML
+    private Rectangle rec49;
+    @FXML
+    private Rectangle rec50;
+    @FXML
+    private Rectangle rec51;
+    @FXML
+    private Rectangle rec52;
+    @FXML
+    private Rectangle rec53;
+    @FXML
+    private Rectangle rec54;
+    @FXML
+    private Rectangle rec55;
+    @FXML
+    private Rectangle rec56;
+    @FXML
+    private Rectangle rec57;
+    @FXML
+    private Rectangle rec58;
+    @FXML
+    private Rectangle rec59;
+    @FXML
+    private Rectangle rec60;
+    @FXML
+    private Rectangle rec61;
+    @FXML
+    private Rectangle rec62;
+    @FXML
+    private Rectangle rec63;
+    @FXML
+    private Rectangle rec64;
+    @FXML
+    private Rectangle rec65;
+    @FXML
+    private Rectangle rec66;
+    @FXML
+    private Rectangle rec67;
+    @FXML
+    private Rectangle rec68;
+    @FXML
+    private Rectangle rec69;
+    @FXML
+    private Rectangle rec70;
+    @FXML
+    private Rectangle rec71;
+    @FXML
+    private Rectangle rec72;
+    @FXML
+    private Rectangle rec73;
+    @FXML
+    private Rectangle rec74;
+    @FXML
+    private Rectangle rec75;
+    @FXML
+    private Rectangle rec76;
+    @FXML
+    private Rectangle rec77;
+    @FXML
+    private Rectangle rec78;
+    @FXML
+    private Rectangle rec79;
+    @FXML
+    private Rectangle rec80;
+    @FXML
+    private Rectangle rec81;
+    @FXML
+    private Rectangle rec82;
+    @FXML
+    private Rectangle rec83;
+    @FXML
+    private Rectangle rec84;
+    @FXML
+    private Rectangle rec85;
+    @FXML
+    private Rectangle rec86;
+    @FXML
+    private Rectangle rec87;
+    @FXML
+    private Rectangle rec88;
+    @FXML
+    private Rectangle rec89;
+    @FXML
+    private Rectangle rec90;
+    @FXML
+    private Rectangle rec91;
+    @FXML
+    private Rectangle rec92;
+    @FXML
+    private Rectangle rec93;
+    @FXML
+    private Rectangle rec94;
+    @FXML
+    private Rectangle rec95;
+    @FXML
+    private Rectangle rec96;
+    @FXML
+    private Rectangle rec97;
+    @FXML
+    private Rectangle rec98;
+    @FXML
+    private Rectangle rec99;
+
     @FXML
     private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+
     }
-    
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }
 }
